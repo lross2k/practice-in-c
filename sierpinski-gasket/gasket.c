90b36242a0e86a15ced579706c0388fee6249239
lross2k <luisross2000@outlook.com>
2021-12-25 16:06:19 -0600

    Implemented Sierpinski Gasket
    - Also small tweak to the makefile

--- sierpinski-gasket/gasket.c
+++ sierpinski-gasket/gasket.c
@@ -1,30 +1,51 @@
 #include <stdio.h>
+
 #include "AD.h"
 
-#define WIDTH 10
-#define HEIGTH 3
+#define MIN_SIZE 1
 
-int main(void)
+void draw_gasket(ad_frm_t *frm, int x, int y, int square_size);
+void sierpinski_gasket(int frame_size);
+
+int main(int argc, char **argv)
+{
+    int it;
+    if (argc <= 1)
+    {
+        sierpinski_gasket(40);
+        return(0);
+    }
+
+    for (it = 1; it < argc; it++)
+        sierpinski_gasket(strtol(argv[it], NULL, 10));
+
+    return(0);
+}
+
+void draw_gasket(ad_frm_t *frm, int x, int y, int square_size)
+{
+    int half_square = 0;
+    if (square_size <= MIN_SIZE)
+    {
+        ad_rect(frm, x, y, square_size, square_size);
+    }
+    else
+    {
+        half_square = square_size/2;
+        draw_gasket(frm, x, y, half_square);
+        draw_gasket(frm, x+half_square, y, half_square);
+        draw_gasket(frm, x+half_square, y+half_square, half_square);
+    }
+}
+
+void sierpinski_gasket(int frame_size)
 {
     /* Testing out the custom library before actually 
      making the sierpinski gasket */
-    ad_frm_t *frm = ad_create(WIDTH, HEIGTH, ' ');
-
+    ad_frm_t *frm = ad_create(frame_size, frame_size, ' ');
+    ad_fill(frm, 'x');
+    draw_gasket(frm, 0, 0, frame_size);
     ad_print(frm);    
     printf("\n");
-
-    ad_fill(frm, '>');
-    ad_tile(frm, 4, 2);
-
-    ad_print(frm);    
-    printf("\n");
-
-    ad_fill(frm, 'o');
-    ad_rect(frm, 0, 0, 2, 2);
-
-    ad_print(frm);    
-    printf("\n");
-
     ad_free(frm);
-    return(0);
 }
